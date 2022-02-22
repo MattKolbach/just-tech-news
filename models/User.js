55721f6cd70edec2974caadffb2fddab0e526eb3
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-21 20:06:12 -0600

    passwords will be hashed, not showing PW is commented out

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
 
 //create User model
 class User extends Model {}
@@ -10,41 +12,53 @@ User.init(
     //TABLE COLUMN DEFINITIONS GO HERE
     //define an id column
     id: {
-        //use the special sequelize DataTypes object provide what type of data it is
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      //use the special sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     //define a username column
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     //define an email column
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        //there cannot be any duplicate email values in this table
-        unique: true,
-        //if allowNull is set to false, we can run our data through validators before creating the table data
-        validate: {
-            isEmail: true
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      //there cannot be any duplicate email values in this table
+      unique: true,
+      //if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true,
+      },
     },
-    //define a passwork column
+    //define a password column
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            //this means the password must be at least four characters long
-            len: [4]
-        }
-    }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //this means the password must be at least four characters long
+        len: [4],
+      },
+    },
   },
+  ///// object 2 follows  /////
   {
+    hooks: {
+      //set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+      },
+      //set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     //TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
     //pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     //don't automatically create crateAt/updatedAt timestamp fields
