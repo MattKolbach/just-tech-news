f7cbbc39ebb2133181dbdc5dbae30b6816adac3d
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-24 12:48:11 -0600

    end 13.4.4

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require('../../config/connection');
 
 /// get all posts ///
 router.get("/", (req, res) => {
@@ -63,6 +64,34 @@ router.post("/", (req, res) => {
     });
 });
 
+/////  PUT /api/posts/upvote ///// (create the vote) MUST be before PUT("/:id")
+router.put('/upvote', (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  }).then(() => {
+    //then find the post we just voted on
+    return Post.findOne({
+      where: {
+        id: req.body.post_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        //use raw MySQL aggregate function query to get a count of how manyvotes the post has and return it under the name `vote_count`
+        [
+          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post_id = vote.post_id)'),
+          'vote_count'
+        ]
+      ]
+    })
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => res.json(err));
+});
+
 ///  put route (update post)  ///
 router.put("/:id", (req, res) => {
   Post.update(
@@ -87,6 +116,8 @@ router.put("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
 
 ///  delete a post  ///
 router.delete('/:id', (req, res) => {
