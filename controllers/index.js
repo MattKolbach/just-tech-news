a6b78ba2bfe206dc9119aeff1e1ee91600cd15a2
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-28 18:05:35 -0600

    created homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const homeroutes = require('./home-routes');
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeroutes);
 
 router.use((req, res) => {
   res.status(404).end();
