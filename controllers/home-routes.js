39bcb19d9907090a5f74ee6182a325e1d3fcc122
Matt Kolbach <mattkolbach@yahoo.com>
2022-03-03 12:31:37 -0600

    conditional views set up, end 14.3.7

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,8 +35,11 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
       //pass a single post object into the homepage template
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render("homepage", { posts });
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -45,13 +48,83 @@ router.get("/", (req, res) => {
 });
 
 /////  login  /////
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-  
-  res.render('login');
+
+  res.render("login");
 });
+
+///// get single post  /////
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      //serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      //pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+////hard coded test post to test endpoint
+// router.get('/post/:id', (req, res) => {
+//   const post = {
+//     id:1,
+//     post_url: "https://handlebarsjs.com/guide/",
+//     title: 'Handlebars Docs',
+//     created_at: new Date(),
+//     vote_count: 10,
+//     Comments: [{}, {}],
+//     user: {
+//       username: 'test_user'
+//     }
+//   };
+
+//   res.render('single-post', { post });
+// });
 
 module.exports = router;
